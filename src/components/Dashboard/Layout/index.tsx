e9504c2446938ed49fe25df78bb405aff5dfe3f6
Kaleb Heitzman <kalebheitzman@gmail.com>
2021-01-19 18:38:32 -0500

    added dashboard

---
+++ src/components/Dashboard/Layout/index.tsx
@@ -0,0 +1,101 @@
+// import libs
+import React, { useContext } from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { config, library } from "@fortawesome/fontawesome-svg-core"
+import { fas } from "@fortawesome/free-solid-svg-icons"
+
+// import components
+import Context from "../Context/Context"
+import GlobalStyles from "./GlobalStyles"
+import Seo from "../Seo"
+import Toggle from "../Toggle"
+import Logo from "../Logo"
+import Topbar from "../Topbar"
+import Navigation from "../Navigation"
+import Footer from "../Footer"
+
+// import styles
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import "@fontsource/work-sans"
+import "@fontsource/quattrocento-sans"
+import styled from "@emotion/styled"
+import mq from "../../utils/media"
+
+// fontawesome config
+config.autoAddCss = false
+library.add(fas)
+
+export const Layout = ({ children, title }) => {
+  const { toggle, navigation } = useContext(Context)
+
+  // static query for logo data
+  const { site: { siteMetadata } } = useStaticQuery(graphql`
+  query SiteLogoQuery {
+    site {
+      siteMetadata {
+        description
+        title
+        siteUrl
+      }
+    }      
+  }
+  `)
+
+  return (
+    <StyledLayout className={toggle ? "toggled" : "untoggled"}>
+      <Seo title={title} />
+      <GlobalStyles />
+      <Toggle />
+      <Logo siteMetadata={siteMetadata} />
+      <Topbar />
+      <Navigation navigation={navigation} />
+      <Main>{children}</Main>
+      <Footer siteMetadata={siteMetadata} />
+    </StyledLayout>
+  )
+}
+
+export default Layout
+
+// StyledLayout component
+const StyledLayout = styled.div`
+  display: grid;
+  grid-template-areas:
+    "logo       logo       topbar"
+    "main       main       main"
+    "sidebar    sidebar    sidebar"
+    "navigation navigation toggle";
+  grid-template-columns: 80px 1fr 90px;
+  grid-template-rows: 80px 1fr 0 80px;
+  height: 100vh;
+  min-height: 100vh;
+  min-height: -webkit-fill-available;
+  max-height: 100vh;
+  max-height: -webkit-fill-available;
+  width: 100vw;
+
+  &.toggled {
+    grid-template-rows: 80px 1fr 1fr 80px;
+  }
+
+  ${mq("tablet_up")} {
+    grid-template-areas:
+      "toggle     logo    topbar"
+      "navigation main    main"
+      "navigation footer  footer";
+    grid-template-columns: 100px var(--sidebar-width) 1fr;
+    grid-template-rows: 80px 1fr 80px;
+  }
+`
+
+// main component
+export const Main = styled.main`
+  grid-area: main;
+  background: var(--content-background);
+  height: calc(100vh - 160px);
+  overflow: hidden;
+
+  ${mq("tablet_up")} {
+    height: calc(100vh - 160px);
+  }
+`
---
+++ src/components/Dashboard/Logo/index.tsx
@@ -0,0 +1,59 @@
+// import libs
+import React from "react"
+
+// import components
+import { Link } from "gatsby"
+
+// import styles
+import styled from "@emotion/styled"
+import mq from "../../utils/media"
+
+// type
+type LogoProps = {
+  siteMetadata: {
+    title: string,
+    description: string,
+    url: string  
+  }
+}
+
+export default ({ siteMetadata }: LogoProps) => {
+  return (
+    <Logo>
+      <h1>
+        <Link to="/">
+          <span>{siteMetadata.title}</span>
+        </Link>
+      </h1>
+    </Logo>
+  )
+}
+
+// logo component
+const Logo = styled.div`
+  grid-area: logo;
+
+  background: var(--logo-background);
+  color: var(--logo-color);
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+  height: 80px;
+
+  h1 {
+    margin: 0;
+    padding: 0;
+    color: var(--logo-color);
+    line-height: 1;
+    font-size: 1.65rem;
+
+    a {
+      text-decoration: none;
+      color: var(--logo-color);
+    }
+  }
+
+  ${mq("tablet_up")} {
+    padding: 0 2rem;
+  }
+`
